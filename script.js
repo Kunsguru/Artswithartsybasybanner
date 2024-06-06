8cc4c74331274f4bacece2e6c4e37008853127e1
Kunsguru <kingsleym2024@gmail.com>
2024-06-06 10:53:55 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+function handleClick() {
+    alert('Redirecting You, Our dearest customer to our Official Webpage...');
+    window.location.href = 'https://artswithartsybasy-gt-rfyy.onrender.com/';
+}
